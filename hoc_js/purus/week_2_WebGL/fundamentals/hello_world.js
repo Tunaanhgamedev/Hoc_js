1c9df538e0bbd226efcae13bffe7fa4455e09a32
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-06-14 23:59:15 +0700

    Rendering Triangles

--- hoc_js/purus/week_2_WebGL/fundamentals/hello_world.js
+++ hoc_js/purus/week_2_WebGL/fundamentals/hello_world.js
@@ -19,12 +19,7 @@
 //         doSomethingWith_gl_Position();
 //     }
 // }
-
-var canvas = document.querySelector("#canvas");
-var gl = canvas.getContext("webgl");
-if (!gl) {
-  console.log("WebGL not supported");
-}
+"use strict";
 
 function createShader(gl, type, source) {
   var shader = gl.createShader(type);
@@ -37,12 +32,6 @@ function createShader(gl, type, source) {
   console.log(gl.getShaderInfoLog(shader));
   gl.deleteShader(shader);
 }
-
-var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-
-var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
 function createProgram(gl, vertexShader, fragmentShader) {
   var program = gl.createProgram();
@@ -57,14 +46,65 @@ function createProgram(gl, vertexShader, fragmentShader) {
   gl.deleteProgram(program);
 }
 
-var program = createProgram(gl, vertexShader, fragmentShader);
+function main() {
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    console.log("WebGL not supported");
+    return;
+  }
 
-var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+  var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
 
-var positionBuffer = gl.createBuffer();
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource,
+  );
 
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var program = createProgram(gl, vertexShader, fragmentShader);
 
-// three 2d points
-var positions = [0, 0, 0, 0.5, 0.7, 0];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  var positionBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // three 2d points
+  var positions = [0, 0, 0, 0.5, 0.7, 0];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  //webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.useProgram(program);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  var size = 2; // 2 components per iteration
+  var type = gl.FLOAT; // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset,
+  );
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+}
+
+main();
