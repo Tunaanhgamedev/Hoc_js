c6cb6b49ed7220b19aa426a24246ca1a235025d2
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-06-24 01:47:36 +0700

    webgl 2d: matrices p1(indentity)

---
+++ hoc_js/purus/week_2_WebGL/WebGL_Learning/2D/04_2d_matrices/main.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl");
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(
+  gl,
+  gl.FRAGMENT_SHADER,
+  fragmentShaderSource,
+);
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+
+const positionLocation = gl.getAttribLocation(program, "a_position");
+
+const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+const colorLocation = gl.getUniformLocation(program, "u_color");
+const matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+const translationMatrix = translation(50, 100);
+const rotationMatrix = rotation(0);
+const scaleMatrix = scaling(0.5, 0.5);
+
+let matrix = m3.multiply(translationMatrix, rotationMatrix);
+matrix = m3.multiply(matrix, scaleMatrix);
+
+const positionBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+setGeometry(gl);
+
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+gl.clear(gl.COLOR_BUFFER_BIT);
+gl.useProgram(program);
+gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const size = 2; // 2 components per iteration
+const type = gl.FLOAT; // the data is 32bit floats
+const normalize = false; // don't normalize the data
+const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+const offset = 0; // start at the beginning of the buffer
+gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
+
+gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+gl.uniform4fv(colorLocation, [Math.random(), Math.random(), Math.random(), 1]);
+
+gl.uniformMatrix3fv(matrixLocation, false, matrix);
+
+const primitiveType = gl.TRIANGLES;
+gl.drawArrays(primitiveType, 0, 18);
+
+function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      // left column
+      0, 0, 30, 0, 0, 150, 0, 150, 30, 0, 30, 150,
+
+      // top rung
+      30, 0, 100, 0, 30, 30, 30, 30, 100, 0, 100, 30,
+
+      // middle rung
+      30, 60, 67, 60, 30, 90, 30, 90, 67, 60, 67, 90,
+    ]),
+    gl.STATIC_DRAW,
+  );
+}
--- hoc_js/purus/week_2_WebGL/WebGL_Learning/2D/04_2d_matrices/shader.js
+++ hoc_js/purus/week_2_WebGL/WebGL_Learning/2D/04_2d_matrices/shader.js
@@ -2,16 +2,15 @@ const vertexShaderSource = `
 attribute vec2 a_position;
 
 uniform vec2 u_resolution;
-uniform vec2 u_translation;
+uniform mat3 u_matrix;
 
 void main(){
-    vec2 position = a_position+ u_translation;
-
+    vec2 position = (u_matrix * vec3(a_position, 1)).xy;
     vec2 zeroToOne = position / u_resolution;
-    vec2 zeroToTwo = zeroToOne*2.0;
-    vec2 clipSpace = zeroToTwo-1.0;
+    vec2 zeroToTwo = zeroToOne * 2.0;
+    vec2 clipSpace = zeroToTwo - 1.0;
 
-    gl_Position = vec4(clipSpace * vec2(1.0,-1.0),0.0,1.0);
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
