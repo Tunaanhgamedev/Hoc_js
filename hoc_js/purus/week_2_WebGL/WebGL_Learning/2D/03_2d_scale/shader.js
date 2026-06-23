ce0e13fe011d7f4c1dcf2c564f98f29964929086
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-06-24 00:46:11 +0700

    webgl 2d: scale

--- hoc_js/purus/week_2_WebGL/WebGL_Learning/2D/03_2d_scale/shader.js
+++ hoc_js/purus/week_2_WebGL/WebGL_Learning/2D/03_2d_scale/shader.js
@@ -3,15 +3,25 @@ attribute vec2 a_position;
 
 uniform vec2 u_resolution;
 uniform vec2 u_translation;
+uniform vec2 u_rotation;
+uniform vec2 u_scale;
 
 void main(){
-    vec2 position = a_position+ u_translation;
+    // Scale the position
+    vec2 scaledPosition = a_position * u_scale;
+    
+    // Rotate the position
 
+    vec2 rotatedPosition = vec2(
+        scaledPosition.x * u_rotation.y + scaledPosition.y * u_rotation.x,
+        scaledPosition.y * u_rotation.y - scaledPosition.x * u_rotation.x);
+    
+    vec2 position = rotatedPosition + u_translation;
     vec2 zeroToOne = position / u_resolution;
-    vec2 zeroToTwo = zeroToOne*2.0;
-    vec2 clipSpace = zeroToTwo-1.0;
+    vec2 zeroToTwo = zeroToOne * 2.0;
+    vec2 clipSpace = zeroToTwo - 1.0;
 
-    gl_Position = vec4(clipSpace * vec2(1.0,-1.0),0.0,1.0);
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
 `;
 
