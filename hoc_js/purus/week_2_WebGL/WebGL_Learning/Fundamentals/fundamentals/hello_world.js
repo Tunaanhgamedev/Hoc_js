fb8e53496045e1aa3fed10b486e8141eaccbefac
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-06-29 01:07:12 +0700

    webgl fundamentals: draw 50 random rectangles

--- hoc_js/purus/week_2_WebGL/WebGL_Learning/Fundamentals/fundamentals/hello_world.js
+++ hoc_js/purus/week_2_WebGL/WebGL_Learning/Fundamentals/fundamentals/hello_world.js
@@ -84,7 +84,7 @@ function main() {
   gl.useProgram(program);
   gl.enableVertexAttribArray(positionAttributeLocation);
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
   gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
   var resolutionUniformLocation = gl.getUniformLocation(
@@ -98,50 +98,50 @@ function main() {
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
   //   // draw
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 6;
-  //gl.uniform4f(colorUniformLocation, 1, 0, 0, 1);
-  gl.drawArrays(primitiveType, offset, count);
+  // var primitiveType = gl.TRIANGLES;
+  // var offset = 0;
+  // var count = 6;
+  // gl.uniform4f(colorUniformLocation, 1, 0, 0, 1);
+  // gl.drawArrays(primitiveType, offset, count);
 
   //draw 50 random rectangles in random colors
-  // for (var ii = 0; ii < 50; ++ii) {
-  //   setRectangle(
-  //     gl,
-  //     randomInt(300),
-  //     randomInt(300),
-  //     randomInt(300),
-  //     randomInt(300),
-  //   );
+  for (var ii = 0; ii < 50; ++ii) {
+    setRectangle(
+      gl,
+      randomInt(300),
+      randomInt(300),
+      randomInt(300),
+      randomInt(300),
+    );
 
-  //   // Set a random color.
-  //   gl.uniform4f(
-  //     colorUniformLocation,
-  //     Math.random(),
-  //     Math.random(),
-  //     Math.random(),
-  //     1,
-  //   );
+    // Set a random color.
+    gl.uniform4f(
+      colorUniformLocation,
+      Math.random(),
+      Math.random(),
+      Math.random(),
+      1,
+    );
 
-  //   // Draw the rectangle.
-  //   gl.drawArrays(gl.TRIANGLES, 0, 6);
-  // }
-  // function randomInt(range) {
-  //   return Math.floor(Math.random() * range);
-  // }
+    // Draw the rectangle.
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
+  function randomInt(range) {
+    return Math.floor(Math.random() * range);
+  }
 
-  // function setRectangle(gl, x, y, width, height) {
-  //   var x1 = x;
-  //   var x2 = x + width;
-  //   var y1 = y;
-  //   var y2 = y + height;
+  function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
 
-  //   gl.bufferData(
-  //     gl.ARRAY_BUFFER,
-  //     new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-  //     gl.STATIC_DRAW,
-  //   );
-  // }
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+      gl.STATIC_DRAW,
+    );
+  }
 
   // gl.bufferData(
   //   gl.ARRAY_BUFFER,
