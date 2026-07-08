b7ef642052dcf9e30fbb4124904e15ef9255f36b
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-07-08 11:07:03 +0700

    learn pixijs: Core concepts (Garbage Collection: managing textures with unload)

--- hoc_js/purus/week_5_PixiJs/Getting_Started/Render_Layers/src/garbage_Collection.js
+++ hoc_js/purus/week_5_PixiJs/Getting_Started/Render_Layers/src/garbage_Collection.js
@@ -1,6 +1,10 @@
-import { Sprite } from 'pixi.js';
+import { Sprite, Texture } from "pixi.js";
 
+const texture = Texture.from("/assets/bunny.png");
 const sprite = new Sprite(texture);
 
 // Xóa hoàn toàn sprite khỏi bộ nhớ và giải phóng tài nguyên liên quan, xóa ở phía texture lẫn trong gpu
 sprite.destroy();
+
+// Giải phóng tài nguyên liên quan đến texture, xóa ở phía texture lẫn trong gpu
+texture.unload();
